9e0b26f0410e72bfaa8a33eae4f5728ca244e198
bbrabus1159-hue <b.brabus1159@gmail.com>
2026-04-24 06:14:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kintore-v1';
+const CACHE = 'kintore-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
